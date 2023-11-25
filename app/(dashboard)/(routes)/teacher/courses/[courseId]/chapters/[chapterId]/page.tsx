098d71c1657af0439f0bcae51117b823ba503a66
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-11-25 23:09:44 +0530

    Created chapter title form component and customize chapter page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,9 +1,11 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import { ArrowLeft, LayoutDashboard } from "lucide-react";
 
 import { db } from "@/lib/db";
-import Link from "next/link";
-import { ArrowLeft } from "lucide-react";
+import { IconBadge } from "@/components/icon-badge";
+import { ChapterTitleForm } from "./_components/chapter-title-form";
 
 const ChapterIdPage = async ({
   params,
@@ -51,6 +53,30 @@ const ChapterIdPage = async ({
             <ArrowLeft className="h-4 w-4 mr-2" />
             Back to course setup
           </Link>
+          <div className="flex items-center justify-between w-full">
+            <div className="flex flex-col gap-y-2">
+              <h1 className="text-2xl font-medium">Chapter creation</h1>
+              <span className="text-sm text-slate-700">
+                Complete all fields {completionText}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div className="space-y-4">
+          {/* ChapterTitleForm */}
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboard} />
+              <h2 className="text-xl">Customize your chapter</h2>
+            </div>
+            <ChapterTitleForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+          </div>
         </div>
       </div>
     </div>
