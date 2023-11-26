a2bb42e2f3c5fd463a9324b8ea6ad28d2fe592dc
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-11-26 13:23:52 +0530

    dev. chapter deletion function  & chapter delete route

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import { Ban, CheckCircleIcon, Trash } from "lucide-react";
+import toast from "react-hot-toast";
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
+import { Ban, CheckCircleIcon, Trash } from "lucide-react";
+import axios from "axios";
+
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 import { ConfirmModal } from "@/components/modals/confirm-modal";
+import { useRouter } from "next/navigation";
 
 interface ChapterActionsProps {
   disabled: boolean;
@@ -20,15 +24,32 @@ export const ChapterActions = ({
   chapterId,
   isPublished,
 }: ChapterActionsProps) => {
+  const router = useRouter();
+
   const [isLoading, setIsLoading] = useState(false);
 
-  const onDelete = () => {};
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+
+      toast.success("Chapter deleted successfully.");
+
+      router.refresh();
+      router.push(`/teacher/courses/${courseId}`);
+    } catch {
+      toast.error("Something went wrong.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div className="flex items-center gap-x-2">
       <Button
         onClick={() => {}}
-        disabled={disabled}
+        disabled={disabled || isLoading}
         variant={"outline"}
         size={"sm"}
         className={cn(isPublished ? "bg-green-600 text-white" : "bg-white")}
@@ -40,8 +61,8 @@ export const ChapterActions = ({
         )}
         {isPublished ? "Published" : "Publish"}
       </Button>
-      <ConfirmModal onConfirm={() => {}}>
-        <Button size={"sm"}>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size={"sm"} disabled={isLoading}>
           <Trash className="h-4 w-4 mr-1" />
           Delete
         </Button>
