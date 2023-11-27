fc395ad2611dcceb6a0018df1d3f399e9101f823
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-11-27 18:37:40 +0530

    Dev: search input component & created debounce hook for delay in the search api request

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
 
+import { SearchInput } from "@/components/search-input";
+
 const SearchPage = async () => {
   const categories = await db.category.findMany({
     orderBy: {
@@ -9,9 +11,15 @@ const SearchPage = async () => {
   });
 
   return (
-    <div className="p-6">
-      <Categories items={categories} />
-    </div>
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
+    </>
   );
 };
 
