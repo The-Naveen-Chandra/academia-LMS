84a174d3eb083ff51b580eca3ac5ca6a64a3e5b4
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-11-27 20:33:01 +0530

    Dev. courses list component for getting all the courses

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,13 +1,36 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
+import { getCourses } from "@/actions/get-courses";
 
 import { SearchInput } from "@/components/search-input";
+import { CoursesList } from "@/components/courses-list";
 
-const SearchPage = async () => {
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
+  });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
   });
 
   return (
@@ -18,6 +41,7 @@ const SearchPage = async () => {
 
       <div className="p-6">
         <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
