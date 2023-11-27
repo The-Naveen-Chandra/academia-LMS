67881b1c40db6234f04e7d583e85b64897011279
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-11-28 01:06:43 +0530

    Dev. getChapter action function & ChapterIdPage component

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,63 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { getChapter } from "@/actions/get-chapter";
+import { Banner } from "@/components/banner";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: {
+    courseId: string;
+    chapterId: string;
+  };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    chapterId: params.chapterId,
+    courseId: params.courseId,
+  });
+
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
+  return (
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner
+          variant={"success"}
+          label="You already completed this chapter."
+        />
+      )}
+      {isLocked && (
+        <Banner
+          variant={"warning"}
+          label="You need to purchase this course to access this chapter."
+        />
+      )}
+      <div className="flex flex-col max-w-4xl mx-auto pb-20">
+        <div className="p-4">{/* Video Player */}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ChapterIdPage;
