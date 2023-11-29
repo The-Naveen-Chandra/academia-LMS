bd8dc8a9c512797f669fa8a611d21f8ac834fde0
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-11-30 01:18:36 +0530

    Styling dashboard page & added course list

---
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -0,0 +1,27 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { getDashboardCourses } from "@/actions/get-dashboard-courses";
+import { CoursesList } from "@/components/courses-list";
+
+export default async function Dashboard() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { completedCourses, coursesInProgress } = await getDashboardCourses(
+    userId
+  );
+
+  return (
+    <div className="p-6 space-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div>{/* // TODO: Info Card */}</div>
+        <div>{/* // TODO: Info Card */}</div>
+      </div>
+      <CoursesList items={[...coursesInProgress, ...completedCourses]} />
+    </div>
+  );
+}
